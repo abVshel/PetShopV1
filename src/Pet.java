46c7474480bf6d6239130ddf5f65e86d14ec2c9d
abVshel <abrashavosanshel@gmail.com>
2025-12-23 21:51:00 +0700

    add pet change string

--- src/Pet.java
+++ src/Pet.java
@@ -1,5 +1,5 @@
 public class Pet {
-    String jenis, waktu, hewan, kategori, item;
+    String waktu, hewan, kategori, item;
     double harga;
 
     public Pet(String w, String h, String k, String i, double hg) {

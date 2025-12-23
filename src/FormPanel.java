def5d8594cd874a444e79897e6e3bf76679563b7
abVshel <abrashavosanshel@gmail.com>
2025-12-23 21:41:40 +0700

    add formpanel & style

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -48,10 +48,10 @@ public class FormPanel extends JPanel {
                 txtHarga.setText(String.valueOf(list.get(idx).price));
         });
 
-        JButton btnSave = new JButton("💾 Simpan");
+        JButton btnSave = new JButton("Simpan");
         FormStyle.save(btnSave);
 
-        JButton btnBack = new JButton("⬅ Kembali");
+        JButton btnBack = new JButton("Kembali");
         FormStyle.back(btnBack);
 
         btnSave.addActionListener(e -> saveData());
